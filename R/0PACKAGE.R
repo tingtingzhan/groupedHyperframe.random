cdbd027c234112f525e092863f197f8020e9a141
Tingting Zhan <tingtingzhan@gmail.com>
2026-03-16 08:49:10 -0400

    tzh

--- R/0PACKAGE.R
+++ R/0PACKAGE.R
@@ -8,3 +8,23 @@
 '_PACKAGE'
 
 
+
+
+#' @importFrom utils citation
+.onAttach <- function(libname, pkgname) {
+  
+  # .onAttach(libname = 'lib', pkgname = 'groupedHyperframe') # nah..
+  
+  'groupedHyperframe.random' |>
+    citation() |>
+    format(style = 'text') |> # utils:::format.citation
+    col_green() |>
+    style_bold() |>
+    cli_inform(class = 'packageStartupMessage')
+  
+  '\n' |>
+    cli_inform(class = 'packageStartupMessage')
+  
+}
+
+
